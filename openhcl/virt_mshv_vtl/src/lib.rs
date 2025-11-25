335159fbce6cac6bd5c9f9eca9a57c88fc713381
Flgodd67 <b-bearam@microsoft.com>
2025-11-25 15:16:55 +0000

    still trying

--- openhcl/virt_mshv_vtl/src/lib.rs
+++ openhcl/virt_mshv_vtl/src/lib.rs
@@ -44,7 +44,7 @@ mod processor;
 use aarch64defs::Vendor;
 #[cfg(not(guest_arch = "x86_64"))]
 use hcl::GuestVtl;
-use hcl::ioctl::IsolationType;
+// use hcl::ioctl::IsolationType;
 #[cfg(guest_arch = "aarch64")]
 use hcl::ioctl::cca::RsiRealmConfig;
 #[cfg(guest_arch = "x86_64")]
@@ -2160,10 +2160,10 @@ impl UhProtoPartition<'_> {
     /// Whether Guest VSM is available to the guest. If so, for hardware CVMs,
     /// it is safe to expose Guest VSM support via cpuid.
     fn check_guest_vsm_support(hcl: &Hcl) -> Result<bool, Error> {
-        if hcl.isolation() == hcl::ioctl::IsolationType::Cca {
-            // TODO: CCA: will need to let it go through the checks below in the future.
-            return Ok(true);
-        }
+        // if hcl.isolation() == hcl::ioctl::IsolationType::Cca {
+        //     // TODO: CCA: will need to let it go through the checks below in the future.
+        //     return Ok(true);
+        // }
         #[cfg(guest_arch = "x86_64")]
         let privs = {
             let result = safe_intrinsics::cpuid(hvdef::HV_CPUID_FUNCTION_MS_HV_FEATURES, 0);
