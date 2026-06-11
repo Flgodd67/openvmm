b1c6db64b89a8c8a8d0a9414d4358674fddea5aa
Flgodd67 <b-bearam@microsoft.com>
2026-06-11 11:59:38 +0100

    cca: adding a new location where the AArch64 tests can be placed: tmk/simple_tmk/src/aarch64. Keeping architecture specific code and tests grouped together

---
+++ tmk/simple_tmk/src/aarch64/mod.rs
@@ -0,0 +1,73 @@
+
+#![cfg(target_arch = "aarch64")]
+
+#![allow(unsafe_code)]
+use core::arch;
+
+use crate::prelude::*;
+
+core::arch::global_asm! {
+    ".global instruction_abort_outside_par_trampoline",
+    "instruction_abort_outside_par_trampoline:",
+    "movz x16, #0x0000",
+    "movk x16, #0x0000, lsl #16",
+    "movk x16, #0xffff, lsl #32",
+    "movk x16, #0x0000, lsl #48",
+    "br x16",
+}
+
+unsafe extern "C" {
+    fn instruction_abort_outside_par_trampoline() -> !;
+}
+
+#[tmk_test]
+fn instruction_abort_outside_par(_: TestContext<'_>) {
+    log!("instruction_abort_outside_par");
+
+    unsafe {
+        instruction_abort_outside_par_trampoline();
+    }
+
+    // panic!("branch to outside PAR unexpectedly returned");
+}
+
+core::arch::global_asm! {
+    ".global instruction_abort_ripas_empty_a",
+    "instruction_abort_ripas_empty_a:",
+    "movz x16, #0x0000",
+    "br x16",
+}
+
+unsafe extern "C" {
+    fn instruction_abort_ripas_empty_a() -> !;
+}
+
+#[tmk_test]
+fn instruction_abort_ripas_empty(_: TestContext<'_>) {
+    log!("instruction_abort_ripas_empty");
+
+    unsafe {
+        instruction_abort_ripas_empty_a();
+    }
+}
+
+core::arch::global_asm! {
+    ".global instruction_abort_permissions_enabled_a",
+    "instruction_abort_permissions_enabled_a:",
+    "movz x16, #0xf000",
+    "movk x16, #0x847f, lsl #16",
+    "br x16",
+}
+
+unsafe extern "C" {
+    fn instruction_abort_permissions_enabled_a() -> !;
+}
+
+#[tmk_test]
+fn instruction_abort_permissions_enabled(_: TestContext<'_>) {
+    log!("instruction_abort_permissions_enabled");
+
+    unsafe {
+        instruction_abort_permissions_enabled_a();
+    }
+}
--- tmk/simple_tmk/src/common.rs
+++ tmk/simple_tmk/src/common.rs
@@ -2,78 +2,10 @@
 // Licensed under the MIT License.
 
 //! Simple tests common to all architectures.
-#![allow(unsafe_code)]
-use core::arch;
 
 use crate::prelude::*;
 
 #[tmk_test]
 fn boot(_: TestContext<'_>) {
     log!("hello world");
-}
-
-// core::arch::global_asm! {
-//     ".global instruction_abort_outside_par_trampoline",
-//     "instruction_abort_outside_par_trampoline:",
-//     "movz x16, #0x0000",
-//     "movk x16, #0x0000, lsl #16",
-//     "movk x16, #0xffff, lsl #32",
-//     "movk x16, #0x0000, lsl #48",
-//     "br x16",
-// }
-
-// unsafe extern "C" {
-//     fn instruction_abort_outside_par_trampoline() -> !;
-// }
-
-// #[tmk_test]
-// fn instruction_abort_outside_par(_: TestContext<'_>) {
-//     log!("instruction_abort_outside_par");
-
-//     unsafe {
-//         instruction_abort_outside_par_trampoline();
-//     }
-
-//     // panic!("branch to outside PAR unexpectedly returned");
-// }
-
-// core::arch::global_asm! {
-//     ".global instruction_abort_ripas_empty_a",
-//     "instruction_abort_ripas_empty_a:",
-//     "movz x16, #0x0000",
-//     "br x16",
-// }
-
-// unsafe extern "C" {
-//     fn instruction_abort_ripas_empty_a() -> !;
-// }
-
-// #[tmk_test]
-// fn instruction_abort_ripas_empty(_: TestContext<'_>) {
-//     log!("instruction_abort_ripas_empty");
-
-//     unsafe {
-//         instruction_abort_ripas_empty_a();
-//     }
-// }
-
-core::arch::global_asm! {
-    ".global instruction_abort_permissions_enabled_a",
-    "instruction_abort_permissions_enabled_a:",
-    "movz x16, #0xf000",
-    "movk x16, #0x847f, lsl #16",
-    "br x16",
-}
-
-unsafe extern "C" {
-    fn instruction_abort_permissions_enabled_a() -> !;
-}
-
-#[tmk_test]
-fn instruction_abort_permissions_enabled(_: TestContext<'_>) {
-    log!("instruction_abort_permissions_enabled");
-
-    unsafe {
-        instruction_abort_permissions_enabled_a();
-    }
 }
